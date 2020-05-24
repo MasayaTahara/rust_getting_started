373d746a70d055b3da1f0918ec2b0251e11296b0
masaya.tahara <mocoso.0u1@gmail.com>
2020-05-24 13:58:35 +0900

    Add ownership test

---
+++ session_3/ownership/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Q.1
+    let s = String::from("test");
+    let s2 = s;
+    println!("s: {}", s);
+    println!("s2: {}", s2);
+
+    // Q.2
+    let s = "test";
+    let s2 = s;
+    println!("s: {}", s);
+    println!("s2: {}", s2);
+
+    // Q.3
+    let s = String::from("Hello") ;
+    // println!("{}", string_length());
+}
