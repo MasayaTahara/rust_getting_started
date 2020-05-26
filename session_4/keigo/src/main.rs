8c87cdade9083c293a8e8e15a3a43102008c2683
masaya.tahara <mocoso.0u1@gmail.com>
2020-05-26 09:49:21 +0900

    Fix function name

--- session_4/keigo/src/main.rs
+++ session_4/keigo/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    // Q. 以下の日本語の語尾「です。」「ます。」を「である。」に変換する関数を作りなさい。
+    // Q. 以下の日本語の語尾「です。」を「である。」に変換する関数を作りなさい。
     let s1 = "埼玉は日本の首都です。";
     let s2 = "私の実家は埼玉です。";
 
-    println!("{}", desumasu_to_dearu(s1));
-    println!("{}", desumasu_to_dearu(s2));
+    println!("{}", desu_to_dearu(s1));
+    println!("{}", desu_to_dearu(s2));
 }
 
-fn desumasu_to_dearu(?) -> ? {
+fn desu_to_dearu(?) -> ? {
 }
