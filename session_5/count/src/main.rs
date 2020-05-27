f557b6bd092763cdccba9ecf4d48f13dd10a08f9
masaya.tahara <mocoso.0u1@gmail.com>
2020-05-27 21:10:53 +0900

    Add session5 test

---
+++ session_5/count/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Q. 以下の文字列中に含まれる任意のアルファベットを数える関数を作りなさい
+    let s = "Saitma is my home town!!";
+    // ex.
+    // println!("{}", count_char(s, "a"));
+    // -> 2が出力される
+}
+
+fn count_char(?) -> ? {
+}
