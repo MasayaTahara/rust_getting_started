e4a4981cda63c55440aad2efb00bcf2f829017ca
masaya.tahara <mocoso.0u1@gmail.com>
2020-05-28 18:45:45 +0900

    Add session6 test

---
+++ session_6/question/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Q. 以下の数字の中で重複を取り除いた時に何個の数字が残るか求める関数を作りなさい。
+    let n = 387587386;
+    // 例
+    println!("{}", get_number_of_single(n));
+
+}
+
+fn get_number_of_single() -> {
+}
