a4af1818ff999f80d14d11bcfb272bf183f21dea
masaya.tahara <mocoso.0u1@gmail.com>
2020-05-28 18:48:05 +0900

    Fix question

--- session_6/question/src/main.rs
+++ session_6/question/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
     // Q. 以下の数字の中で重複を取り除いた時に何個の数字が残るか求める関数を作りなさい。
     let n = 387587386;
-    // 例
     println!("{}", get_number_of_single(n));
-
+    // 出力 -> 5
 }
 
 fn get_number_of_single() -> {
